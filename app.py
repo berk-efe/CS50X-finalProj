efdcd6ffcd5506d6a3bd972f782907d0d51c4f8d
berk-efe <berkefekeskin@gmail.com>
2025-06-22 11:30:43 +0300

    added login and register pages, finished register view, created a database, gonna modify the navbar further

--- app.py
+++ app.py
@@ -2,17 +2,29 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helper import DEFAULT_SHOPS
-from helper import get_deals
+from helper import get_deals, login_required
 
 # Configure application
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///keepTrack.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route('/')
 def index():
@@ -64,3 +76,58 @@ def games():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    if request.method == 'POST':
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            flash("must provide username", "danger")
+            return redirect("/register")
+
+        elif not email:
+            flash("must provide email", "danger")
+            return redirect("/register")
+
+        elif not password:
+            flash("must provide password", "danger")
+            return redirect("/register")
+        
+        elif not confirmation:
+            flash("must provide confirmation", "danger")
+            return redirect("/register")
+        
+        elif password != confirmation:
+            flash("passwords do not match", "danger")
+            return redirect("/register")
+        
+        try:
+            db.execute(
+                "INSERT INTO users(username, email, hash) VALUES(?,?,?)",
+                username, email, generate_password_hash(password)
+            )
+
+            rows = db.execute(
+                "SELECT * FROM users WHERE username = ?", username
+            )
+
+            session["user_id"] = rows[0]["id"]
+            flash('Registration Complete!', 'success')
+            return redirect('/')
+
+        except ValueError as e:
+            flash(f"Username already exists\n {e}", "danger")
+            return redirect("/register")
+        
+    
+    return render_template('register.html')
+
--- helper.py
+++ helper.py
@@ -3,6 +3,9 @@ import os
 import json
 import requests
 
+from flask import redirect, render_template, session
+from functools import wraps
+
 from typing import Union
 
 DEFAULT_SHOPS = [61, 16, 35, 48]  # Steam, Epic Games, GOG, Microsoft Store
@@ -112,3 +115,17 @@ def get_deals(sort:str="-trending", limit:int=6, shops:list[int]=DEFAULT_SHOPS, 
 
     return data if data else None
 
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
